a85d58b51428da9acb94db05f4c535e39ee7e8cb
nguyentran186 <nguyen@NGUYEN>
2024-12-12 22:56:13 +0700

    ?

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/DUE-web/', // Update this to match your repository's GitHub Pages path
+  base: '/DUE-web', // Update this to match your repository's GitHub Pages path
 });
